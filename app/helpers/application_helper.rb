712a654ee95e329948196d15a0d480b75066bd4b
yaroslav gubich <yar.mansens@gmail.com>
2024-06-07 04:56:58 +0200

    Create: logo -generated simple logo and pasted into navbar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,10 @@
 # app/helpers/application_helper.rb
 module ApplicationHelper
   def bootstrap_class_for(flash_type)
-    case flash_type
-    when 'notice'
+    case flash_type.to_sym
+    when :notice
       'success'   # Bootstrap class for success messages
-    when 'alert'
-      'danger'    # Bootstrap class for error messages
-    when 'error'
+    when :alert, :error
       'danger'    # Bootstrap class for error messages
     else
       flash_type.to_s
