a628eeefdcc16c9e35968028bb9551cb490f8e5a
cd ~/code/$GITHUB_USERNAME/dotfiles && zsh git_setup.sh <dimitrisreth86@gmail.com>
2024-06-07 14:15:23 +0200

    ok

--- app/models/gear.rb
+++ app/models/gear.rb
@@ -3,4 +3,9 @@ class Gear < ApplicationRecord
   has_many :bookings, dependent: :destroy
 
   validates :title, :description, :price_per_day, :category, :availability, presence: true
+  # added this line so if the gear is owned by the user he cannot book it and he see the delete and edit
+  def owned_by?(user)
+    self.user == user
+  end
+
 end
